186193cd05a5af9a87b0330fee99fc396bbad9ea
frieren000 <31646071@qq.com>
2025-09-15 23:08:45 +0800

    完善第一个接口,并取消一些测试文件

--- Python/django/blog/views.py
+++ Python/django/blog/views.py
@@ -1,5 +1,4 @@
 import pymysql
-from django.http import JsonResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from tools import base_tools
@@ -12,27 +11,64 @@ def hello(request):
         'message': 'Hello from Django',
         'status': 'success',
     }
-    return JsonResponse(hello_dict)
+    return Response(hello_dict)
 
 @api_view(['GET'])
 def search_users_info(request):
     # 查找用户信息
-    connect = pymysql.connect(**base_tools.database_config)
-    cursor = connect.cursor()
+    data_list, columns = [], []
     
-    search_user_sql = f'''
-    SELECT * FROM users;
-    '''
-    cursor.execute(search_user_sql)
-    rows = cursor.fetchall()
-    data = list(rows)
-    cursor.close()
-    connect.close()
     
-    data_content_dict = {
-        'code': 200,
-        'message': 'success',
-        'data': data,
-    }
-    
-    return JsonResponse(data_content_dict)
+    try:
+        search_user_sql = f'''
+        select * form users where 1 = 1
+        '''
+        params = []
+        user_id = request.GET.get('id')
+        name = request.GET.get('name')
+        email = request.GET.get('email')
+        
+        if user_id:
+            search_user_sql += 'and id = %s'
+            params.append(user_id)
+        
+        if name:
+            search_user_sql += 'and name like %s'
+            params.append(f'%{name}%')
+        
+        if email:
+            search_user_sql += 'and email = %s'
+            params.append(email)
+        
+        connect = pymysql.connect(**base_tools.database_config)
+        cursor = connect.cursor()
+        cursor.execute(search_user_sql)
+        
+        for col in cursor.description:
+            columns.append(col[0])
+            
+        rows = cursor.fetchall()
+        data_list = [dict(zip(columns, row)) for row in rows]
+        
+        data_content_dict = {
+            'code': 200,
+            'message': 'success',
+            'data': data_list,
+        }
+        status = 200
+        
+    except Exception as e:
+        data_content_dict = {
+            'code':500,
+            'message': 'failed',
+            'data': str(e),
+        }
+        status = 500
+        
+    finally:
+        if cursor:
+            cursor.close()
+        if connect:
+            connect.close()
+            
+    return Response(data_content_dict,status=status)
