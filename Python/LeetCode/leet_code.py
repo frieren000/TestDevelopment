89a171f6bb0fa8162426a46c6b84f56318877101
frieren000 <31646071@qq.com>
2025-10-12 21:35:20 +0800

    增加两个LC

--- Python/LeetCode/leet_code.py
+++ Python/LeetCode/leet_code.py
@@ -39,13 +39,28 @@ def isPalindrome(int):
     else:
         return False
 
-# 3.两数相加 -- 待尝试
-# def addTwoNumbers(list_1, list_2):
-#     return 0
-    
-# l1 = [9,9,9,9,9,9,9]
-# l2 = [9,9,9,9]
-# addTwoNumbers(l1, l2)
+# 3.两数相加 -- 双指针解法
+def ListNode(val=0, next=None):
+         val = val
+         next = next
+def addTwoNumbers(l1, l2):
+        dummy = ListNode() # 虚拟头节点
+        head = dummy
+        
+        carry = 0
+        while l1 or l2 or carry:
+            if l1:
+                carry += l1.val
+                l1 = l1.next
+            if l2:
+                carry += l2.val
+                l2 = l2.next
+            node = ListNode(carry % 10)
+            carry //= 10
+            dummy.next = node
+            dummy = dummy.next
+        
+        return head.next
 
 # 4.删除有序数组中的重复项
 def removeDuplicates(nums):
@@ -669,4 +684,43 @@ def numOfSubarrays(arr, k, threshold):
         if current_sums >= threshold * k:
             substr_num += 1
     
-    return substr_num        
+    return substr_num
+
+# 32. 魔法序列的数组乘积之和 -- 有点过于抽象
+MOD = 1_000_000_007
+MX = 31
+
+fac = [0] * MX  # fac[i] = i!
+fac[0] = 1
+for i in range(1, MX):
+    fac[i] = fac[i - 1] * i % MOD
+
+inv_f = [0] * MX  # inv_f[i] = i!^-1
+inv_f[-1] = pow(fac[-1], -1, MOD)
+for i in range(MX - 1, 0, -1):
+    inv_f[i - 1] = inv_f[i] * i % MOD
+
+def magicalSum(m, k, nums):
+    n = len(nums)
+    pow_v = [[1] * (m + 1) for _ in range(n)]
+    for i, v in enumerate(nums):
+        for j in range(1, m + 1):
+            pow_v[i][j] = pow_v[i][j - 1] * v % MOD
+
+    @cache
+    def dfs(i, left_m, x, left_k):
+        c1 = x.bit_count()
+        if c1 + left_m < left_k:  # 可行性剪枝
+            return 0
+        if i == n or left_m == 0 or left_k == 0:  # 无法继续选数字
+            return 1 if left_m == 0 and c1 == left_k else 0
+        res = 0
+        for j in range(left_m + 1):  # 枚举 I 中有 j 个下标 i
+            # 这 j 个下标 i 对 S 的贡献是 j * pow(2, i)
+            # 由于 x = S >> i,转化成对 x 的贡献是 j
+            bit = (x + j) & 1  # 取最低位,提前从 left_k 中减去,其余进位到 x 中
+            r = dfs(i + 1, left_m - j, (x + j) >> 1, left_k - bit)
+            res += r * pow_v[i][j] * inv_f[j]
+        return res % MOD
+
+    return dfs(0, m, 0, k) * fac[m] % MOD
