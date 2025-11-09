0aa3051130406d6146d9a3f723fc83eca1561d72
王羽佳 <31646071@qq.com>
2025-11-09 16:57:46 +0800

    LC Days

--- Python/LeetCode/leet_code.py
+++ Python/LeetCode/leet_code.py
@@ -1318,3 +1318,15 @@ def minimumOneBitOperations(n: int) -> int:
         ans ^= n
         n //= 2
     return ans
+
+# 66. 得到0的操作数
+def countOperations(num1: int, num2: int) -> int:
+        count = 0
+        while num1 != 0 and num2 != 0:
+            if num1 >= num2:
+                num1 -= num2
+            else:
+                num2 -= num1
+            count += 1
+            
+        return count
