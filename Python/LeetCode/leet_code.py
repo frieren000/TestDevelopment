b3e778e257b203bf04e04e186a3ae59a829a6f25
王羽佳 <31646071@qq.com>
2025-11-21 22:18:22 +0800

    LC Days

--- Python/LeetCode/leet_code.py
+++ Python/LeetCode/leet_code.py
@@ -1488,3 +1488,25 @@ def findFinalValue(nums: List[int], original: int) -> int:
             flag = False
     
     return original
+
+# 76. 设置交集大小至少为2
+def intersectionSizeTwo(intervals: List[List[int]]) -> int:
+        intervals.sort(key=lambda interval: interval[1])
+        # 栈中保存闭区间左右端点，栈底到栈顶的区间长度的和
+        st = [(-2, -2, 0)]  # 哨兵，保证不和任何区间相交
+        for start, end in intervals:
+            _, r, s = st[bisect.bisect_left(st, (start,)) - 1]
+            d = 2 - (st[-1][2] - s)  # 去掉运行中的时间点
+            if start <= r:  # start 在区间 st[i] 内
+                d -= r - start + 1  # 去掉运行中的时间点
+            if d <= 0:
+                continue
+            while end - st[-1][1] <= d:  # 剩余的 d 填充区间后缀
+                l, r, _ = st.pop()
+                d += r - l + 1  # 合并区间
+            st.append((end - d + 1, end, st[-1][2] + d))
+        return st[-1][2]
+
+# 77. 长度为 3 的不同回文子序列
+def countPalindromicSubsequence(s: str) -> int:
+        return sum(len(set(s[s.index(c)+1:s.rindex(c)])) for c in set(s) if s.rindex(c)-s.index(c)>1) 
